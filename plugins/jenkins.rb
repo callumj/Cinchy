c622271b3d56caf8e214d1275bf85dc07836f3e5
Callum Jones <callum@callumj.com>
2012-01-10 12:43:04 +0800

    Fixing Jenkins status info

--- plugins/jenkins.rb
+++ plugins/jenkins.rb
@@ -22,14 +22,18 @@ module BotPlugins
         response["jobs"].each do |job|
           job_status = HTTParty.get("#{job["url"]}/api/json", options)
           last_success = job_status["lastSuccessfulBuild"].nil? ? nil : job_status["lastSuccessfulBuild"]["number"]
+          last_fail = job_status["lastUnsuccessfulBuild"].nil? ? nil : job_status["lastUnsuccessfulBuild"]["number"]
           current_build = job_status["lastBuild"].nil? ? nil : job_status["lastBuild"]["number"]
-          status = current_build.eql?(last_success) ? "stable" : "failing"
+          status = current_build.eql?(last_success) ? "stable" : "unknown"
+          status = "failing" if (current_build.eql?(last_fail) || last_fail > last_success)
           run_time = ""
+          in_progress = false
           if (job_status["lastBuild"] && job_status["lastBuild"]["url"])
             last_run = HTTParty.get("#{job_status["lastBuild"]["url"]}/api/json", options)
             run_time = "Executed at #{Time.at(last_run["timestamp"]/1000).to_s}"
+            in_progress = last_run["building"]
           end
-          m.reply("Job '#{job_status["name"]}'#{job_status["inQueue"] ? "(in progress)" : ""} is #{status}")
+          m.reply("Job '#{job_status["name"]}'#{in_progress ? "(in progress)" : ""} is #{status}")
           m.reply("   #{run_time}") if run_time.length > 0
           m.reply("   #{job_status["healthReport"].last["description"]}")
         end
